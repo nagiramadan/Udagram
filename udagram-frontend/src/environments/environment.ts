141055abcb493f5aa50bcea615536a383e792eed
Nagi Ramadan <nagiramadan@users.noreply.github.com>
2022-10-05 03:28:13 +0200

    deploy to k8s

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af28dddf8fb634f0d8cc3aba0c65240d-910493392.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
